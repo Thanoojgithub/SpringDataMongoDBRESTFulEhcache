fa65450498e84c05522a9589c4b89ae3216dc3c6
PN00019 <Thanooj.Kalathuru@hibu.com>
2015-04-08 20:42:34 +0530

    ehcache code changes

--- src/main/java/com/springrest/CustomerServiceImpl.java
+++ src/main/java/com/springrest/CustomerServiceImpl.java
@@ -1,10 +1,22 @@
 package com.springrest;
 
-import org.apache.commons.lang3.text.WordUtils;
+import java.util.Date;
 
+import org.apache.commons.lang3.text.WordUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerServiceImpl implements CustomerService {
 
+	final static Logger logger = LoggerFactory.getLogger(CustomerServiceImpl.class);
+
+	@Cacheable(value = "customerServiceCache", key = "#name")
 	public String sayHello(String name) {
-		return "Hello " + WordUtils.capitalize(name) + ", openShift welcomes you.";
+		Date date = new Date();
+		logger.debug("sayHello : "+name +" at : "+date);
+		return "Hello " + WordUtils.capitalize(name) + ", openShift welcomes you. TIMESTAMP :: "+date;
 	}
 }
