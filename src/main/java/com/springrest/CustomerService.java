15afb95c1f2759df74c3738db65edeaab3e24012
PN00019 <Thanooj.Kalathuru@hibu.com>
2015-04-09 16:36:53 +0530

    using same cache name for multiple methods

--- src/main/java/com/springrest/CustomerService.java
+++ src/main/java/com/springrest/CustomerService.java
@@ -6,6 +6,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.cache.annotation.Cacheable;
+
 
 @Path("/services")
 public interface CustomerService {
@@ -15,6 +17,18 @@ public interface CustomerService {
     @GET
     @Path("/sayHello")
     @Produces(MEDIATYPE_CHARSET)
+    @Cacheable(value = "customerServiceCache", key = "#name")
     public String sayHello(@QueryParam("name") String name);
+    
+    @GET
+    @Path("/sayBye")
+    @Produces(MEDIATYPE_CHARSET)
+    @Cacheable(value = "customerServiceCache", key = "#name")
+    public String sayBye(@QueryParam("name") String name);
+    
+    @GET
+    @Path("/beInTouch")
+    @Produces(MEDIATYPE_CHARSET)
+    @Cacheable(value = "customerServiceCache", key = "#email")
+    public String beInTouch(@QueryParam("email") String email);
 }
-
--- src/main/java/com/springrest/CustomerServiceImpl.java
+++ src/main/java/com/springrest/CustomerServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import org.apache.commons.lang3.text.WordUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,10 +12,23 @@ public class CustomerServiceImpl implements CustomerService {
 
 	final static Logger logger = LoggerFactory.getLogger(CustomerServiceImpl.class);
 
-	@Cacheable(value = "customerServiceCache", key = "#name")
 	public String sayHello(String name) {
 		Date date = new Date();
 		logger.debug("sayHello : "+name +" at : "+date);
 		return "Hello " + WordUtils.capitalize(name) + ", openShift welcomes you. TIMESTAMP :: "+date;
+	}
+
+	@Override
+	public String sayBye(String name) {
+		Date date = new Date();
+		logger.debug("sayBye : "+name +" at : "+date);
+		return "Bye " + WordUtils.capitalize(name) + ", openShift welcomes you. TIMESTAMP :: "+date;
+	}
+
+	@Override
+	public String beInTouch(String email) {
+		Date date = new Date();
+		logger.debug("beInTouch : "+email +" at : "+date);
+		return "beInTouch " + email + ", openShift welcomes you. TIMESTAMP :: "+date;
 	}
 }
