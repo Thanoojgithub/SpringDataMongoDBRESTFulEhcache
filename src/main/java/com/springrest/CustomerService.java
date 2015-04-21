4fb42e4b6c65c99ca4c8fdc427b2c25a49fc16c5
Thanoojgithub <javaeecub@gmail.com>
2015-04-21 07:53:55 +0530

    @Aysnc related code changes

--- src/main/java/com/springrest/CustomerService.java
+++ src/main/java/com/springrest/CustomerService.java
@@ -1,5 +1,7 @@
 package com.springrest;
 
+import java.util.concurrent.Future;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,6 +9,8 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Path("/services")
@@ -15,20 +19,21 @@ public interface CustomerService {
 	public static final String MEDIATYPE_CHARSET = MediaType.APPLICATION_JSON + ";charset=utf-8";
 
     @GET
+    @Async
     @Path("/sayHello")
     @Produces(MEDIATYPE_CHARSET)
     @Cacheable(value = "customerServiceCache", key = "#name")
-    public String sayHello(@QueryParam("name") String name);
+    public @ResponseBody Future<String> sayHello(@QueryParam("name") String name);
     
     @GET
     @Path("/sayBye")
     @Produces(MEDIATYPE_CHARSET)
     @Cacheable(value = "customerServiceCache", key = "#name")
-    public String sayBye(@QueryParam("name") String name);
+    public @ResponseBody String sayBye(@QueryParam("name") String name);
     
     @GET
     @Path("/beInTouch")
     @Produces(MEDIATYPE_CHARSET)
     @Cacheable(value = "customerServiceCache", key = "#email")
-    public String beInTouch(@QueryParam("email") String email);
+    public @ResponseBody String beInTouch(@QueryParam("email") String email);
 }
--- src/main/java/com/springrest/CustomerServiceImpl.java
+++ src/main/java/com/springrest/CustomerServiceImpl.java
@@ -1,10 +1,13 @@
 package com.springrest;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.Future;
 
 import org.apache.commons.lang3.text.WordUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,20 +15,23 @@ public class CustomerServiceImpl implements CustomerService {
 
 	final static Logger logger = LoggerFactory.getLogger(CustomerServiceImpl.class);
 
-	public String sayHello(String name) {
-		Date date = new Date();
-		logger.debug("sayHello : "+name +" at : "+date);
-		return "Hello " + WordUtils.capitalize(name) + ", openShift welcomes you. TIMESTAMP :: "+date;
+	public Future<String> sayHello(String name) {
+		try {
+			logger.debug("sayHello before sleep : "+name +" at : "+Calendar.getInstance().getTime());
+			Thread.sleep(6000);
+			logger.debug("sayHello after sleep: "+name +" at : "+Calendar.getInstance().getTime());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return new AsyncResult<String>("Hello " + WordUtils.capitalize(name) + ", openShift welcomes you. TIMESTAMP :: "+Calendar.getInstance().getTime());
 	}
 
-	@Override
 	public String sayBye(String name) {
 		Date date = new Date();
 		logger.debug("sayBye : "+name +" at : "+date);
 		return "Bye " + WordUtils.capitalize(name) + ", openShift welcomes you. TIMESTAMP :: "+date;
 	}
 
-	@Override
 	public String beInTouch(String email) {
 		Date date = new Date();
 		logger.debug("beInTouch : "+email +" at : "+date);
