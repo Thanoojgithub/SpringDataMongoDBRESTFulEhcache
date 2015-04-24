a209adb9ac934c73c90e0bd2634a07c356793754
PN00019 <Thanooj.Kalathuru@hibu.com>
2015-04-24 21:53:07 +0530

    added MongoDB CRUD operations

--- src/main/java/com/springrest/CustomerService.java
+++ src/main/java/com/springrest/CustomerService.java
@@ -1,6 +1,8 @@
 package com.springrest;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -9,41 +11,36 @@ import javax.ws.rs.core.MediaType;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.springrest.vo.User;
 
 @Path("/services")
 public interface CustomerService {
-	
+
 	public static final String MEDIATYPE_CHARSET = MediaType.APPLICATION_JSON + ";charset=utf-8";
 
-    /*@GET
-    @Async
-    @Path("/sayHello")
-    @Produces(MEDIATYPE_CHARSET)
-    @Cacheable(value = "customerServiceCache", key = "#name")
-    public @ResponseBody Future<String> sayHello(@QueryParam("name") String name);
-    */
-    
 	@GET
-    @Path("/sayHello")
-    @Produces(MEDIATYPE_CHARSET)
-    @Cacheable(value = "customerServiceCache", key = "#name")
-    public @ResponseBody String sayHello(@QueryParam("name") String name);
-	
-    @GET
-    @Path("/sayBye")
-    @Produces(MEDIATYPE_CHARSET)
-    @Cacheable(value = "customerServiceCache", key = "#name")
-    public @ResponseBody String sayBye(@QueryParam("name") String name);
-    
-    @GET
-    @Path("/beInTouch")
-    @Produces(MEDIATYPE_CHARSET)
-    @Cacheable(value = "customerServiceCache", key = "#email")
-    public @ResponseBody String beInTouch(@QueryParam("email") String email);
-    
-    @GET
-    @Path("/statistics")
-    public void getCacheStatistics();
-    
-    
+	@Path("/sayHello")
+	@Produces(MEDIATYPE_CHARSET)
+	@Cacheable(value = "customerServiceCache", key = "#name")
+	public @ResponseBody String sayHello(@QueryParam("name") String name);
+
+	@GET
+	@Path("/getUser")
+	@Produces(MEDIATYPE_CHARSET)
+	public @ResponseBody User getUser(@QueryParam("id") String id);
+
+	@DELETE
+	@Path("/deleteUser")
+	@Produces(MEDIATYPE_CHARSET)
+	public @ResponseBody User deleteUser(@QueryParam("id") String id);
+
+	@PUT
+	@Path("/upsertUser")
+	@Produces(MEDIATYPE_CHARSET)
+	public @ResponseBody User upsertUser(@QueryParam("id")String id, @QueryParam("firstName")String firstName, @QueryParam("lastName")String lastName, @QueryParam("location")String location);
+
+	@GET
+	@Path("/statistics")
+	public void getCacheStatistics();
+
 }
--- src/main/java/com/springrest/CustomerServiceImpl.java
+++ src/main/java/com/springrest/CustomerServiceImpl.java
@@ -1,7 +1,6 @@
 package com.springrest;
 
 import java.util.Calendar;
-import java.util.Date;
 
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
@@ -14,58 +13,36 @@ import org.apache.commons.lang3.text.WordUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Component;
+
+import com.springrest.vo.User;
 
 @Component
 public class CustomerServiceImpl implements CustomerService {
 
-	final static Logger logger = LoggerFactory
-			.getLogger(CustomerServiceImpl.class);
+	final static Logger logger = LoggerFactory.getLogger(CustomerServiceImpl.class);
 
 	@Autowired
 	private CacheManager cacheManager;
+	
+	@Autowired
+	private MongoOperations mongoOperations;
 
 	public String sayHello(String name) {
-		logger.debug("sayHello before sleep : " + name + " at : "
-				+ Calendar.getInstance().getTime());
+		logger.debug("sayHello before sleep : " + name + " at : "+ Calendar.getInstance().getTime());
 		try {
 			Thread.sleep(6000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		logger.debug("sayHello after sleep: " + name + " at : "
-				+ Calendar.getInstance().getTime());
-		String str = new String("Hello " + WordUtils.capitalize(name)
-				+ ", openShift welcomes you. TIMESTAMP :: "
+		logger.debug("sayHello after sleep: " + name + " at : "+ Calendar.getInstance().getTime());
+		String str = new String("Hello " + WordUtils.capitalize(name) + ", openShift welcomes you. TIMESTAMP :: "
 				+ Calendar.getInstance().getTime());
 		return str;
-	}
-
-	/*
-	 * public Future<String> sayHello(String name) { try {
-	 * logger.debug("sayHello before sleep : "+name
-	 * +" at : "+Calendar.getInstance().getTime()); Thread.sleep(6000);
-	 * logger.debug("sayHello after sleep: "+name
-	 * +" at : "+Calendar.getInstance().getTime()); } catch
-	 * (InterruptedException e) { e.printStackTrace(); } String str = new
-	 * String("Hello " + WordUtils.capitalize(name) +
-	 * ", openShift welcomes you. TIMESTAMP :: "
-	 * +Calendar.getInstance().getTime()); return new AsyncResult<String>(str);
-	 * }
-	 */
-
-	public String sayBye(String name) {
-		Date date = new Date();
-		logger.debug("sayBye : " + name + " at : " + date);
-		return "Bye " + WordUtils.capitalize(name)
-				+ ", openShift welcomes you. TIMESTAMP :: " + date;
-	}
-
-	public String beInTouch(String email) {
-		Date date = new Date();
-		logger.debug("beInTouch : " + email + " at : " + date);
-		return "beInTouch " + email + ", openShift welcomes you. TIMESTAMP :: "
-				+ date;
 	}
 
 	public void getCacheStatistics() {
@@ -76,5 +53,34 @@ public class CustomerServiceImpl implements CustomerService {
 		}
 		
 	}
+	
+	@Override
+	public User getUser(String id) {
+		return mongoOperations.findById(id, User.class);
+	}
+	
+	@Override
+	public User deleteUser(String id) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("id").is(id));
+		User findAndRemove = mongoOperations.findAndRemove(query, User.class);
+		return findAndRemove;
+	}
 
+	@Override
+	public User upsertUser(String id, String firstName, String lastName, String location) {
+		System.out.println("upsertUser :: "+id + " "+firstName +" "+lastName+" "+location);
+		Query query = new Query();
+		query.addCriteria(Criteria.where("id").is(id));
+		Update update = new Update();
+		update.set("id", id);
+		update.set("firstName", firstName);
+		update.set("lastName", lastName);
+		update.set("location", location);
+		System.out.println(query + " "+update);
+		mongoOperations.upsert(query, update, User.class);
+		User user= mongoOperations.findById(id, User.class);
+		System.out.println("user :: " + user);
+		return user;
+	}
 }
